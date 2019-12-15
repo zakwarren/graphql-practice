016e74f4a2eb29461d1b5653c7a18ee325ed0f02
zakwarren <41547855+zakwarren@users.noreply.github.com>
2019-12-15 12:28:22 +0000

    error handling

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
@@ -18,22 +20,30 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            message: 'Validation failed, entered data is incorrect',
-            errors: errors.array()
-        });
+        const error = new Error('Validation failed, entered data is incorrect');
+        error.statusCode = 422;
+        throw error;
     }
 
     const title = req.body.title;
     const content = req.body.content;
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: {
-            _id: new Date().toISOString().replace(/:/g, '-'),
-            title: title,
-            content: content,
-            creator: { name: 'Ace' },
-            createdAt: new Date()
-        }
+    const post = new Post({
+        title: title,
+        imageUrl: 'images/fox.jpg',
+        content: content,
+        creator: { name: 'Ace' }
     });
+    post.save()
+        .then(result => {
+            res.status(201).json({
+                message: 'Post created successfully!',
+                post: result
+            });
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
