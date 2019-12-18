d176501f1399f7be9cc7464e74b1b27add0a66cf
zakwarren <41547855+zakwarren@users.noreply.github.com>
2019-12-18 21:03:56 +0000

    user model

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const credentials = require('./credentials');
 const fileUpload = require('./middleware/file-upload');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -26,6 +27,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 app.use((error, req, res, next) => {
     console.log(error);
