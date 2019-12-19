5e61b2b3516dd5dc04d589de6b8264092b133895
zakwarren <41547855+zakwarren@users.noreply.github.com>
2019-12-19 13:44:07 +0000

    update post using websocket

--- controllers/feed.js
+++ controllers/feed.js
@@ -125,13 +125,13 @@ exports.updatePost = async (req, res, next) => {
     }
 
     try {
-        const post = await Post.findById(postId)
+        const post = await Post.findById(postId).populate('creator');
         if (!post) {
             const error = new Error('Could not find post');
             error.statusCode = 404;
             throw error;
         }
-        if (post.creator.toString() !== req.userId) {
+        if (post.creator._id.toString() !== req.userId) {
             const error = new Error('Not authorized!');
             error.statusCode = 403;
             throw error;
@@ -144,6 +144,11 @@ exports.updatePost = async (req, res, next) => {
         post.imageUrl = imageUrl;
         post.content = content;
         const result = await post.save();
+
+        io.getIo().emit('posts', {
+            action: 'update',
+            post: result
+        });
 
         res.status(200).json({
             message: 'Post updated',
