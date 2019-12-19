f3353c67abb0181f6e4ba78354af33c47b922a77
zakwarren <41547855+zakwarren@users.noreply.github.com>
2019-12-19 17:06:34 +0000

    login with graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const credentials = require('../credentials');
 const validation = require('./validation');
 
 const User = require('../models/user');
@@ -26,6 +28,33 @@ module.exports = {
         return {
             ...createdUser._doc,
             _id: createdUser._id.toString()
+        };
+    },
+    login: async function({ email, password}) {
+        const user = await User.findOne({ email: email });
+        if (!user) {
+            const error = new Error('Authentication failed');
+            error.code = 401;
+            throw error;
+        }
+        const isEqual = await bcrypt.compare(password, user.password);
+        if (!isEqual) {
+            const error = new Error('Authentication failed');
+            error.code = 401;
+            throw error;
+        }
+
+        const token = jwt.sign(
+            {
+                userId: user._id.toString(),
+                email: user.email
+            },
+            credentials.PRIVATE_KEY,
+            { expiresIn: '1h' }
+        );
+        return {
+            token: token,
+            userId: user._id.toString()
         };
     }
 };
