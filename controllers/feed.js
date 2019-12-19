40db993053e986af486cc9f0451d684770fae296
zakwarren <41547855+zakwarren@users.noreply.github.com>
2019-12-19 10:25:35 +0000

    create post attached to user

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
     const currentPage = req.query.page || 1;
@@ -48,17 +49,30 @@ exports.createPost = (req, res, next) => {
     const title = req.body.title;
     const content = req.body.content;
     const imageUrl = req.file.path.replace("\\" ,"/");
+    let creator;
     const post = new Post({
         title: title,
         content: content,
         imageUrl: imageUrl,
-        creator: { name: 'Ace' }
+        creator: req.userId
     });
     post.save()
-        .then(result => {
+        .then(() => {
+            return User.findById(req.userId);
+        })
+        .then(user => {
+            creator = user;
+            user.posts.push(post);
+            return user.save();
+        })
+        .then(() => {
             res.status(201).json({
                 message: 'Post created successfully!',
-                post: result
+                post: post,
+                creator: {
+                    _id: creator._id,
+                    name: creator.name
+                }
             });
         })
         .catch(err => {
@@ -151,7 +165,7 @@ exports.deletePost = (req, res, next) => {
                 throw error;
             }
             clearImage(post.imageUrl);
-            return Post.findByIdAndRemove(postId);
+            return Post.deleteOne({ _id: postId });
         })
         .then(() => {
             res.status(200).json({
